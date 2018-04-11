3d37a2a1cad4ae433e01e8b81de2e107dd912f87
Ginger Lee Kretschmer <kretschmons@gmail.com>
2018-04-11 12:22:29 -0700

    add in past year data for application exception for top 5

--- src/components/Airbrake.js
+++ src/components/Airbrake.js
@@ -2,6 +2,8 @@ import React from 'react';
 import Chartist from 'chartist';
 import ChartistGraph from 'react-chartist';
 import 'chartist-plugin-tooltips';
+import ChartistPluginLegend from 'chartist-plugin-legend';
+
 
 class Airbrake extends React.Component {
   render() {
@@ -9,17 +11,21 @@ class Airbrake extends React.Component {
     const data = {
       labels: ['2009', '2012', '2014', '2016', '2018'],
       series: [
-        {'name': 'NewRelic', 'data': [0, 0, 0, 0, 20] },
-        {'name': 'Rollbar', 'data': [0, 0, 0, 0, 14] },
-        {'name': 'Sentry', 'data': [0, 0, 0, 0, 14] },
-        {'name': 'Airbrake', 'data': [36, 53, 28, 21, 11] },
-        {'name': 'Honeybadger', 'data': [0, 0, 0, 0, 8] }
+        {'name': 'Airbrake', 'data': [36, 53, 28, 17, 11] },
+        {'name': 'NewRelic', 'data': [0, 2, 0, 0, 20] },
+        {'name': 'Rollbar', 'data': [0, 0, 4, 18, 14] },
+        {'name': 'ExceptionNotifier plugin', 'data': [58, 34, 24, 15, 2] },
+        {'name': 'Honeybadger', 'data': [0, 0, 13, 12, 8] }
       ]
     };
     const options = {
       fullWidth: true,
       chartPadding: 20,
       plugins: [
+        ChartistPluginLegend({
+          legendNames: ['Airbrake', 'NewRelic', 'Rollbar', 'ExceptionNotifier', 'Honeybadger'],
+          clickable: true,
+        }),
         Chartist.plugins.tooltip({
           transformTooltipTextFnc: function(value) {
             return value + '%';
