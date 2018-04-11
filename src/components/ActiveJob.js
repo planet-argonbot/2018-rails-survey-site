2630caa4abe1603af85ae049d4fbd99352026712
Holly Wilkalis <hollywilkalis@MacBook-Pro-2.local>
2018-04-11 15:56:52 -0700

    Change library import order, modify chart style on activejob, delete OpsSystems

---
+++ src/components/ActiveJob.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import Chartist from 'chartist';
+import ChartistGraph from 'react-chartist';
+import 'chartist-plugin-tooltips';
+import ChartistPluginLegend from 'chartist-plugin-legend';
+
+
+class ActiveJob extends React.Component {
+  render() {
+
+    const data = {
+      labels: [
+        'SIDEKIQ',
+        'DELAYED JOB',
+        'RESQUE',
+        'ACTIVE ASYNC JOB',
+        'SUCKER PUNCH',
+        'OTHER'],
+      series:
+        [50, 21, 13, 5, 3, 7]
+    };
+
+    const options = {
+      donut: true,
+      donutWidth: 50,
+      chartPadding: 5,
+      donutSolid: true,
+      showLabel: false,
+      height: '300px',
+      plugins: [
+        Chartist.plugins.tooltip({
+          transformTooltipTextFnc: function(value) {
+            return value + '%';
+          }
+        }),
+        ChartistPluginLegend()
+      ]
+    };
+
+    const responsiveOptions = [
+      ['screen and (max-width: 544px)', {
+        height: '200px',
+        donutWidth: 20,
+      }],
+
+      ['screen and (min-width: 545px)', {
+        height: '250px',
+        donutWidth: 30,
+      }]
+    ];
+
+    return (
+      <div>
+        <ChartistGraph
+          className={'ct-custom-donut align-bottom'}
+          data={data}
+          options={options}
+          responsiveOptions={responsiveOptions}
+          type={'Pie'} />
+      </div>
+    );
+  }
+}
+
+export default ActiveJob;
