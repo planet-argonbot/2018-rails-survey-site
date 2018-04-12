0d363b32452c057ecfdc7a89086068c02f362f78
Ginger Lee Kretschmer <kretschmons@gmail.com>
2018-04-12 09:47:36 -0700

    update share buttons to use font awesome icons

--- src/components/Share.js
+++ src/components/Share.js
@@ -5,9 +5,16 @@ function Share(){
   return (
     <div>
       <h4>Share the results with your community!</h4>
-      <button class="btn-yellow"><span class="icon-facebook h5 align-middle"></span> Facebook</button><button class="btn-yellow"><span class="icon-twitter h5 align-middle"></span> Twitter</button>
+      <div class="margin-top-md text-center">
+        <a href="#" class="btn-yellow margin-right-sm js-facebook-trigger" target="_blank"><i class="fa fa-facebook-official margin-right-xs"></i>Facebook</a>
+        <a href="#" class="btn-yellow js-twitter-trigger" target="_blank"><i class="fa fa-twitter-square margin-right-xs"></i>Twitter</a>
+      </div>
     </div>
   );
 }
 
 export default Share;
+
+//
+//
+// <button class="btn-yellow"><span class="icon-facebook h5 align-middle"></span> Facebook</button><button class="btn-yellow"><span class="icon-twitter h5 align-middle"></span> Twitter</button>
