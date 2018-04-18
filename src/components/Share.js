b38d501d74af866558a6e332c999e1703c881b87
Ginger Lee Kretschmer <kretschmons@gmail.com>
2018-04-18 15:50:03 -0700

    update twitter popout function and share image

--- src/components/Share.js
+++ src/components/Share.js
@@ -3,10 +3,18 @@ import React from 'react';
 
 function Share(){
 
-  function openShare(event) {
+  function openShareFb(event) {
     event.preventDefault();
     window.open(
       'https://www.facebook.com/sharer/sharer.php?u=http://rails-survey.s3-website-us-west-2.amazonaws.com',
+      'Rails Hosting Survey 2018',
+      'height=400,width=500,toolbar=0,location=0,menubar=0,'
+    );
+  }
+  function openShareTw(event) {
+    event.preventDefault();
+    window.open(
+      'https://twitter.com/share?url=http://rails-survey.s3-website-us-west-2.amazonaws.com/',
       'Rails Hosting Survey 2018',
       'height=400,width=500,toolbar=0,location=0,menubar=0,'
     );
@@ -16,8 +24,8 @@ function Share(){
     <div className="padding-top-xl padding-bottom-xl">
       <h2>Share the results with your community!</h2>
       <div className="padding-sm text-center">
-        <a href="https://www.facebook.com/sharer/sharer.php?u=http://rails-survey.s3-website-us-west-2.amazonaws.com" className="btn-yellow margin-right-sm" target="_blank" onClick={openShare}><i className="fa fa-facebook-official margin-right-xs"></i>Facebook</a>
-        <a href="https://twitter.com/share?url=http://rails-survey.s3-website-us-west-2.amazonaws.com/" className="btn-yellow" target="_blank"><i className="fa fa-twitter-square margin-right-xs"></i>Twitter</a>
+        <a className="btn-yellow margin-right-sm" target="_blank" onClick={openShareFb}><i className="fa fa-facebook-official margin-right-xs"></i>Facebook</a>
+        <a className="btn-yellow" target="_blank" onClick={openShareTw}><i className="fa fa-twitter-square margin-right-xs"></i>Twitter</a>
       </div>
     </div>
   );
